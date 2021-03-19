a135e2746bbcd7a2681d482df35d0a6e1a6cf9d9
Massimiliano Scarpetta <massy1997.lavoro@gmail.com>
2021-03-19 23:02:37 +0100

    Ora il Player può sparare tenendo premuta la barra spaziatrice -> (Utilizzato Coroutine). Creato lo shredder per i colpi sparati a vuoto.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,9 @@ public class Player : MonoBehaviour
     [SerializeField] float padding = 0.5f;
     [SerializeField] GameObject laserPrefab;
     [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.5f;
+
+    Coroutine firingCoroutine;
 
     float xMin;
     float xMax;
@@ -31,20 +34,39 @@ public class Player : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+            firingCoroutine = StartCoroutine(FireContinuously());
+        }
+        if(Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        //TODO - mettere un padding a 2 parametro.
+        while (true)
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
             laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
         }
     }
 
     private void Move()
     {
-        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movement.Normalize();
+        movement *= Time.deltaTime * moveSpeed;
 
-        float newPosX = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
-        float newPosY = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        float playerPosX = transform.position.x + movement.x;
+        float playerPosY = transform.position.y + movement.y;
 
-        transform.position = new Vector2(newPosX, newPosY);
+        transform.position = new Vector2(Mathf.Clamp(playerPosX, xMin, xMax), Mathf.Clamp(playerPosY, yMin, yMax));
+
+        //transform.position = (Vector2)transform.position + movement;
+        //transform.position = new Vector2(Mathf.Clamp(transform.position.x + movement.x, xMin, xMax), Mathf.Clamp(transform.position.y + movement.y, yMin, yMax));
+        //Debug.Log(transform.position.x + " " + transform.position.y);
     }
 
     private void SetUpMoveBoundaries()
