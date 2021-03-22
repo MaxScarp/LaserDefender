35a792fcbd4d9411db2f36517f85ba153b565717
Massimiliano Scarpetta <massy1997.lavoro@gmail.com>
2021-03-22 20:51:41 +0100

    Aggiunto secondo shredder. Aggiunta la vita del player ed ora tutti  i nemici sono in grado di sparare ognuno con il proprio tipo di laser. Aggiunto Background scorrevole e qualche particle effect per simulare qualche stella in vicinanza.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -15,6 +15,8 @@ public class Enemy : MonoBehaviour
     private void ProcessHit(DamageDealer damageDealer)
     {
         health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+
         if (health <= 0)
         {
             Destroy(gameObject);
